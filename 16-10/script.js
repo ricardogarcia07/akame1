ad539c5cdae6474b5e4fe0a2e0f8897a3422610d
ricardogarcia07 <garciamalavericardodavid@gmail.com>
2024-10-17 11:38:10 -0300

    Add files via upload

--- 16-10/script.js
+++ 16-10/script.js
@@ -1,19 +1,38 @@
 const meuParagrafo = document.getElementById('texto');
 const meubotao = document.getElementById('meubotao');
 const meubotao2 = document.getElementById('meubotao2');
+const meubotao3 = document.getElementById('meubotao3');
 
 
 
 
-function alterarTexto() {
-    meuParagrafo.textContent = 'O texto foi alterado!';
-}
+function alterarTexto() {}
+
+meubotao.addEventListener('click', () => {
+    if (!alterarTexto) {
+        meuParagrafo.textContent = 'O texto foi alterado';
+        alterarTexto = true;
+    } else {
+        meuParagrafo.textContent = 'Clique no botão para mudar este texto';
+        alterarTexto = false;
+    }
+});
+
+meubotao.addEventListener('click', alterarTexto);
+
 function alterarCor() {
-    meuParagrafo.style.color = 'red';
+    meuParagrafo.style.color = 'violet';
 }
+function alterarCorDeFundo() {
+    meubotao3.style.backgroundColor = 'darkcyan';
+}
+
+
 
 
 meubotao.addEventListener('click', alterarTexto);
-meubotao2.addEventListener('click', alterarCor)
- 
+meubotao2.addEventListener('click', alterarCor);
+meubotao3.addEventListener('dblclick', alterarCorDeFundo);
+
+
 
