5656de83eec5c0f7eeb0f1e67e8485317522a817
ricardogarcia07 <garciamalavericardodavid@gmail.com>
2024-10-22 11:35:10 -0300

    Add files via upload

--- 21-10/script.js
+++ 21-10/script.js
@@ -1,3 +1,5 @@
+const canvas = document.getElementById('gameCanvas');
+
 const ctx = canvas.getContext('2d');
 
 const box = 20;
@@ -21,8 +23,8 @@ document.addEventListener('keydown', changeDirection);
 function changeDirection(event) {
 
     if (event.keyCode === 37 && direction !== 'RIGHT') {direction =
-    'LEFT';
-}
+        'LEFT';
+    }
     if (event.keyCode === 38 && direction !== 'DOWN') {direction =
         'UP'
     }
@@ -37,3 +39,80 @@ function changeDirection(event) {
 
 }
 
+
+
+
+function draw() {
+    ctx.fillStyle = 'Indigo';
+
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+    for (let i = 0; i < snake.length; i++) {
+
+    if (i === 0) {
+        ctx.fillStyle = 'DarkOrange';
+    } else {
+        ctx.fillStyle = 'white';
+    }
+
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+    ctx.strokeStyle = 'black';
+
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+
+    }
+
+    ctx.fillStyle = 'darkred';
+
+    ctx.strokeStyle = 'black';
+
+    ctx.strokeRect(food.x, food.y, box, box);
+
+    ctx.fillRect(food.x, food.y, box, box);
+
+
+    let snakeX = snake[0].x;
+
+    let snakeY = snake[0].y;
+
+
+    if (direction === 'LEFT') {
+        snakeX -= box;
+    } else if (direction === 'UP') {
+        snakeY -= box;
+    } else if (direction === 'RIGHT') {
+        snakeX += box;
+    } else if (direction === 'DOWN') {
+        snakeY += box;
+    }
+
+
+
+
+
+    if (snakeX === food.x && snakeY === food.y) {
+
+        food = {
+            x: Math.floor(Math.random() * 20) * box,
+            y: Math.floor(Math.random() * 20) * box
+        };
+    } else {
+
+        snake.pop();
+    }
+
+
+    const newHead = { x: snakeX, y: snakeY};
+
+
+    snake.unshift(newHead);
+    
+    
+}
+
+setInterval(draw, 150)
+
+draw()
