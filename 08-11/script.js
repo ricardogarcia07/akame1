cb73ca7ed0f580a9ab4904560de82187a340e767
ricardogarcia07 <garciamalavericardodavid@gmail.com>
2024-11-14 11:32:20 -0300

    Add files via upload

--- 08-11/script.js
+++ 08-11/script.js
@@ -1,37 +1,73 @@
+
+let perguntaAtiva = 0;
+const questoes = document.querySelectorAll('.questao')
+
+function ativar() {
+    questoes[perguntaAtiva].classList.toggle('ativa')
+
+}
+
 function responder(correta) {
     if (correta) {
-        alert('Resposta correta! Indo para próxima pergunta...');
+        // Avançar para a próxima pergunta
+        questoes[perguntaAtiva].classList.remove('ativa')
+        perguntaAtiva += 1
+        ativar()
+       
     } else {
+        // Entrar no modo tela cheia e exibir imagem de erro
         setTimeout(() => {
             goFullScreen();
-            exibirErro();  
-        }, 2000);
+            exibirErro();
+        }, 50);
     }
 }
 
+
+
+
+
+// Função para exibir a imagem de erro e tocar o som
 function exibirErro() {
-    const telaCheiaErro = document.getElementById('telaCheiaErro');
-    const somErro = document.getElementById('somErro');
+    const telaCheiaErro = document.getElementById("telaCheiaErro");
+    const somErro = document.getElementById("somErro");
 
+    telaCheiaErro.style.display = "flex";  // Exibe a imagem em tela cheia
+    somErro.play();  // Toca o som de erro
 
-    telaCheiaErro.style.display = 'flex';
-
-    somErro.play();
-
-    telaCheiaErro.addEventListener('click', () => {
-        telaCheiaErro.style.display = 'none';
+    // Clica na tela para sair do modo de erro
+    telaCheiaErro.addEventListener("click", () => {
+        telaCheiaErro.style.display = "none";
         sairTelaCheia();
-    })
+    });
 }
 
+// Função para entrar no modo tela cheia
 function goFullScreen() {
     const elem = document.documentElement;
     if (elem.requestFullscreen) {
         elem.requestFullscreen();
-        } else if (elem.mozRequestFullScreen) {
-            elem.mozRequestFullScreen();
-        } else if (webkitRequestFullScreen()) {
-            elem.webkitRequestFullScreen();
-        } else if (elem.msRequestFullScreen)
-            elem.msRequestFullScreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari e Opera
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
     }
+}
+
+// Função para sair do modo tela cheia
+function sairTelaCheia() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari e Opera
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+        document.msExitFullscreen();
+    }
+}
+
+
+
